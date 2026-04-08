1ad26f4b3fbf186081de6e667d726f60f6683a33
miro <jarbasai@mailfence.com>
2026-04-08 16:07:42 +0100

    chore: remove deprecated class

--- test/unittests/skills/test_auto_translatable.py
+++ test/unittests/skills/test_auto_translatable.py
@@ -1,6 +1,5 @@
 import unittest
 
-from ovos_workshop.skills.common_query_skill import CommonQuerySkill
 from ovos_workshop.skills.fallback import FallbackSkill
 from ovos_workshop.skills.ovos import OVOSSkill
 
@@ -24,22 +23,5 @@ class TestUniversalFallbackSkill(unittest.TestCase):
         self.assertIsInstance(self.test_skill, self.UniversalFallback)
         self.assertIsInstance(self.test_skill, OVOSSkill)
         self.assertIsInstance(self.test_skill, FallbackSkill)
-
-    # TODO: Test other class methods
-
-
-class TestUniversalCommonQuerySkill(unittest.TestCase):
-    from ovos_workshop.skills.auto_translatable import UniversalCommonQuerySkill
-
-    class UniveralCommonQueryExample(UniversalCommonQuerySkill):
-        def CQS_match_query_phrase(self, phrase):
-            pass
-
-    test_skill = UniveralCommonQueryExample()
-
-    def test_00_init(self):
-        self.assertIsInstance(self.test_skill, self.UniversalCommonQuerySkill)
-        self.assertIsInstance(self.test_skill, OVOSSkill)
-        self.assertIsInstance(self.test_skill, CommonQuerySkill)
 
     # TODO: Test other class methods
